e7534f4b3f2d4d673037f12a99b73ecf717dddc3
BBbrighton <chotiputsilp.r@gmail.com>
2025-08-16 00:11:49 +0200

    easy install fix

--- qr_foundry/public/js/qr_foundry/index.js
+++ qr_foundry/public/js/qr_foundry/index.js
@@ -1,7 +1,2 @@
-// QR Foundry - Main JavaScript Entry Point
-console.log('QR Foundry app loaded');
-
-// Add any global JavaScript functionality here
-frappe.ready(function() {
-    // App initialization code
-});
+window.qr_foundry = window.qr_foundry || {};
+console.debug("qr_foundry: assets loaded");
